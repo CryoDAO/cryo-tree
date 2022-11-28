ce7ff7d37077ea99ecae8fefb50cd10de5464b36
Kai Micah Mills <kaimicahmills@gmail.com>
2022-11-28 11:56:21 -0700

    cryo tree config setup, switch node-sass with sass

--- src/Config.js
+++ src/Config.js
@@ -4,28 +4,28 @@
  */
 
 const Config = {
-  title: 'Tech Tree',
-  subtitle: 'Prototype',
+  title: 'Cryonics Tech Tree',
+  subtitle: 'Alpha v0.1',
   key: [
     {
-      title: 'Example One',
-      color: '#9FC4FF',
+      title: 'Challenge',
+      color: '#C89548',
     },
     {
-      title: 'Example Two',
-      color: '#FFC6FE',
+      title: 'Technology',
+      color: '#B8E986',
     },
     {
-      title: 'Example Three',
-      color: '#FDFFB6',
+      title: 'Off-target Application',
+      color: '#6580E1',
     }
   ],
   axis_enabled: false,
   axis_x: ['Example One', 'Example Two', 'Example Three'],
   axis_y: ['Example A', 'Example B', 'Example C'],
-  github_repo_owner: 'KaiMicahMills',
-  github_repo_name: 'tech-tree',
-  github_base_branch: 'staging',
+  github_repo_owner: 'CryoDAO',
+  github_repo_name: 'cryo-tree',
+  github_base_branch: 'master',
   github_token: process.env.REACT_APP_TOKEN,
   cover_image_url: 'https://fsnone-bb4c.kxcdn.com/wp-content/uploads/2021/10/Biotech-program-image.jpg',
   tree_background_color: '#1D243C'
